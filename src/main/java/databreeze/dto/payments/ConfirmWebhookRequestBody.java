7007be26013802270999984480c227eb6624756c
HuuThanh1610 <nghuuthanh004@gmail.com>
2026-06-08 11:27:41 +0700

    PayOS: add dto for payOS and pom.xml

---
+++ src/main/java/databreeze/dto/payments/ConfirmWebhookRequestBody.java
@@ -0,0 +1,14 @@
+package databreeze.dto.payments;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ConfirmWebhookRequestBody {
+    private String webhookUrl;
+
+    public ConfirmWebhookRequestBody(String webhookUrl) {
+        this.webhookUrl = webhookUrl;
+    }
+}
